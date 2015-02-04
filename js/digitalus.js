0075e872c76817b656438d67558c96e30fdb8ff4
Wolf Maul <wolf@semandra.com>
2015-02-04 11:20:55 -0800

    Fixed code to refresh the viewer so that the overlays work on screen resize

--- js/digitalus.js
+++ js/digitalus.js
@@ -2,7 +2,9 @@
 // Digital Page overlay controls and fixes
 // Semandra - jan 2015
 
-jQuery(document).ready(function() {
+var myCurrentPage;
+
+jQuery(document).ready(function(pageCtl) {
   // Button code to toggle overlay - Semandra
 	  jQuery( "#toggleBtn1" ).click(function() {
 		jQuery( ".toolOverlay" ).css( "visibility", "visible" );
@@ -12,12 +14,42 @@ jQuery(document).ready(function() {
 	  });
 	  
   // Reload window (to reset overlay markup) - Semandra
-/*	  window.addEventListener("resize", pageResetOnResize);
-		  
+	  
+	  window.addEventListener("resize", pageResetOnResize);
+		 
+		console.log ( "Current Page " + myCurrentPage);
 		function pageResetOnResize(zoomData) {
 			  // remove old control
-			  jQuery("#MyPageCtl").empty();
-			  // reload and recalculate the page display
-			  document.pageCtl = jQuery("#MyPageCtl").PageCtl(6, OnPageChange);
-	  	};*/
+			  
+			 console.log(document.pageCtl);
+			  
+			  
+			  
+			 // jQuery("#MyPageCtl").empty();
+			  // reload and recalculate the page display // should use the current page not reset it to first page
+			//  document.pageCtl = jQuery("#MyPageCtl").PageCtl(numberOfPages, OnPageChange);
+			//pageCtl.prototype.SetPage(document.pageCtl.curPage);
+			/*jQuery("#MyPageCtl").change(function() {
+				console.log("change function has been fired");
+				
+					}
+				);*/
+				
+				currentPage = document.pageCtl.curPage; // get the current page number
+				lastPage = document.pageCtl.maxPage; // get the total number of pages
+				
+				
+				
+				if (currentPage == lastPage) { 
+					clearPage = currentPage - 1;
+					console.log("this is the last page");
+					console.log("clearPage " + clearPage);
+					} else {
+						clearPage = currentPage + 1
+						console.log("clearPage " + clearPage);
+						};
+				
+				document.pageCtl.SetPage(clearPage); // sets to a different page -- this triggers the refresh to realign page to size
+				document.pageCtl.SetPage(currentPage); //  resets to the original page 
+	  	};
 });
